6e4578351001648258edd7c9c217aec6d64ac475
Sam Lloyd <bobdoe784@gmail.com>
2021-08-09 17:10:32 -0700

    Files properly linked

---
+++ models/workout.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const workoutSchema = new Schema({
+  day: {
+    type: String,
+    trim: true,
+    required: "What date was the excercise completed",
+  },
+  excercises: [
+    {
+      type: {
+        type: String,
+        required: "What type of excercise",
+      },
+      name: {
+        type: String,
+        required: "What is the name of the excercise",
+      },
+      duration: {
+        type: Number,
+        required: "How long is the excercise done for",
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+    },
+  ],
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
