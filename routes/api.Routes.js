598c65a549829ba685cafa887236f9b0fd6b73ef
Sam Lloyd <bobdoe784@gmail.com>
2021-08-09 22:07:25 -0700

    get route created but need to fix errors

--- routes/api.Routes.js
+++ routes/api.Routes.js
@@ -1,2 +1,11 @@
 const router = require("express").Router();
 const Workout = require("../models/workout");
+
+router.get("/api/workouts", async (req, res) => {
+  try {
+    const workoutData = await Workout.find();
+    res.json(workoutData);
+  } catch {
+    res.status(400).json(err);
+  }
+});
