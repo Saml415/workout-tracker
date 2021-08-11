906068e02e87cc656684540ff3984ba14ae78f5f
Sam Lloyd <bobdoe784@gmail.com>
2021-08-11 09:18:57 -0700

    Add database to heroku

--- server.js
+++ server.js
@@ -10,10 +10,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+mongodb+srv://saml415:password123@cluster0.cflld.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
 // routes
