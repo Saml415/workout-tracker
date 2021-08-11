c1fb2b0bab9c53250ffaf6bb76c564e7842ee5b3
Sam Lloyd <bobdoe784@gmail.com>
2021-08-10 17:28:12 -0700

    Api routes completed

--- routes/api.js
+++ routes/api.js
@@ -10,4 +10,34 @@ router.get("/api/workouts", async (req, res) => {
   }
 });
 
+router.get("/api/workouts/range", async (req, res) => {
+  try {
+    const workoutData = await Workout.find();
+    res.json(workoutData);
+  } catch {
+    res.status(400).json(err);
+  }
+});
+
+router.post("/api/workouts", async (req, res) => {
+  try {
+    const workoutData = await Workout.create(req.body);
+    res.json(workoutData);
+  } catch {
+    res.status(400).json(err);
+  }
+});
+
+router.put("/api/workouts/:id", async (req, res) => {
+  try {
+    const workoutID = req.params.id;
+    const workoutData = await Workout.findByIdAndUpdate(workoutID, {
+      $push: { excercises: req.body },
+    });
+    res.json(workoutData);
+  } catch {
+    res.status(400).json(err);
+  }
+});
+
 module.exports = router;
