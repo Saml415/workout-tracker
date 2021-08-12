fcbcb6c70d776f1266089165a15bb67a9477dd9e
Sam Lloyd <bobdoe784@gmail.com>
2021-08-11 18:36:49 -0700

    Read me added but need to work on total exercise duration still

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# workout-tracker
+# Employee-Tracker
+
+## Table of Contents
+
+- [Descritpion](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+- [License](#license)
+
+## Description
+
+This is the repositroy for an app that allows users to track workouts they have completed. With this code a user can be able to enter a workout they have done and track the amount of weight and amount of time spent working out. The app contains standard front end code as well as backend routes, and a database using MongoDB. All data is saved to the app and is displayed through dynamic graphs.
+
+## Installation Guide
+
+To install, look for e-commerce repo listed on the developer account. Fork the repository to yourr account. Next git clone the repo to your local repository. Finally, open the repo using your gitbash terminal.
+
+## Usage
+
+Top layer contains folders of models, routes, seed data, and the server file. The models folder contains models file called workout.js and and index file used to export the models. All frontend files are located in the public folder. Files named API and HTML routes contains repective routes in the routes folder. Seed data file is in seeders folder.
+
+## Contributing
+
+Created by Saml415
+
+## Tests
+
+No tests necessary.
+
+## Questions
+
+If you have any questions you can contact with though my GitHub profile Saml415 or by email bobdoe784@gmail.com
+
+## License
+
+[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+
+This project is under the MPL 2.0 license.
--- models/workout.js
+++ models/workout.js
@@ -2,42 +2,52 @@ const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
-const workoutSchema = new Schema({
-  day: {
-    type: Date,
-    default: Date.now(),
-    required: "What date was the exercise completed",
-  },
-  exercises: [
-    {
-      type: {
-        type: String,
-        required: "What type of exercise",
-      },
-      name: {
-        type: String,
-        required: "What is the name of the exercise",
-      },
-      duration: {
-        type: Number,
-        required: "How long is the exercise done for",
-      },
-      distance: {
-        type: Number,
-      },
-      weight: {
-        type: Number,
-      },
-      reps: {
-        type: Number,
-      },
-      sets: {
-        type: Number,
-      },
-    },
-  ],
-});
+const opts = { toJSON: { virtuals: true } };
 
+const workoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now(),
+      required: "What date was the exercise completed",
+    },
+    exercises: [
+      {
+        type: {
+          type: String,
+          required: "What type of exercise",
+        },
+        name: {
+          type: String,
+          required: "What is the name of the exercise",
+        },
+        duration: {
+          type: Number,
+          required: "How long is the exercise done for",
+        },
+        distance: {
+          type: Number,
+        },
+        weight: {
+          type: Number,
+        },
+        reps: {
+          type: Number,
+        },
+        sets: {
+          type: Number,
+        },
+      },
+    ],
+  },
+  opts
+);
+
+// workoutSchema.virtual("totalWorkout").get(function () {
+//   return this.workout.reduce((total, exercises) => {
+//     return total + exercises.duration;
+//   }, 0);
+// });
 const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
